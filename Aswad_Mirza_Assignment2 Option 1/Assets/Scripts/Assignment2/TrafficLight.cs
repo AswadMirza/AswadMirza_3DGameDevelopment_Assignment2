0c1060fa3c095c17575cea347d73c9e76118505b
AswadMirza <1mirzaasw@gmail.com>
2020-12-11 21:05:50 -0500

    Implemented Requirement 2

---
+++ Aswad_Mirza_Assignment2 Option 1/Assets/Scripts/Assignment2/TrafficLight.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum TrafficLightStates { 
+    RED,
+    YELLOW,
+    GREEN
+
+}
+public class TrafficLight : MonoBehaviour
+{
+
+    public TrafficLightStates state = TrafficLightStates.GREEN;
+
+    public float InitialSwitchTimer = 5f;
+    private float switchTimer = 5f;
+    NavMeshObstacle obstacle;
+    bool isRed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switchTimer = InitialSwitchTimer;
+        obstacle = gameObject.GetComponent<NavMeshObstacle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switchTimer -= Time.deltaTime;
+
+        if (switchTimer <= 0) {
+            //sets the bool to the opposite value
+            isRed = !isRed;
+            switchTimer = InitialSwitchTimer;
+        }
+
+        if (isRed)
+        {
+            state = TrafficLightStates.RED;
+            obstacle.enabled = true;
+            Debug.LogWarning("RED LIGHT");
+        }
+        else {
+            state = TrafficLightStates.GREEN;
+            obstacle.enabled = false;
+            Debug.LogWarning("GREEN LIGHT");
+        }
+    }
+}
